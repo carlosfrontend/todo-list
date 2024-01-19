dfbd566552304463b0e878bce6becfef3bb327a4
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-01-19 21:25:19 +0100

    Feat: Create sidebar navbar and header design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles/main.css";
 import noTranslateIcons from "./ui/noTranslateIcons";
 import togglePanel from "./ui/togglePanel";
+import logoImg from "./img/logo.svg";
 noTranslateIcons();
 togglePanel();
--- src/ui/togglePanel.js
+++ src/ui/togglePanel.js
@@ -2,15 +2,27 @@ export default function togglePanel() {
   const sidebar = document.querySelector(".sidebar");
   const main = document.querySelector(".main");
   const toggPanelBtn = document.querySelector("#toggPanelBtn");
-  window.addEventListener("resize", () => {
-    if (window.innerWidth <= 700) {
+
+  function checkWindowWidthAndResize() {
+    if (window.innerWidth <= 700 && window.innerHeight <= 761) {
       sidebar.style.display = "none";
       main.style.gridColumn = "1/3";
+      toggPanelBtn.textContent = "menu";
+      return;
     } else {
-      sidebar.style.display = "block";
+      sidebar.style.display = "grid";
       main.style.gridColumn = "2/3";
+      toggPanelBtn.textContent = "menu_open";
+      return;
     }
+  }
+  window.addEventListener("DOMContentLoaded", () => {
+    checkWindowWidthAndResize();
   });
+  window.addEventListener("resize", () => {
+    checkWindowWidthAndResize();
+  });
+
   function handlePanel() {
     toggPanelBtn.classList.toggle("menu-open");
     toggPanelBtn.classList.toggle("menu-close");
@@ -18,7 +30,7 @@ export default function togglePanel() {
       ? (toggPanelBtn.textContent = "menu")
       : (toggPanelBtn.textContent = "menu_open");
     if (toggPanelBtn.textContent === "menu_open") {
-      sidebar.style.display = "block";
+      sidebar.style.display = "grid";
       main.style.gridColumn = "2/3";
     } else {
       sidebar.style.display = "none";
