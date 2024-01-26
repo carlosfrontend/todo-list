156ea9b6957d868e4a3b242b895e5d1367705ec5
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-01-26 13:17:53 +0100

    Feat: Config webpack for to use webpack-merge

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo List",
+      favicon: "./favicon.svg",
+      filename: "index.html",
+      template: path.resolve(__dirname, "src", "index.html"),
+      inject: true,
+    }),
+  ], module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
