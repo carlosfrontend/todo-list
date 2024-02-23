76870e553bb5f36f9a388b7a09aed1a685baaf72
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-02-23 18:55:55 +0100

    Fix: Deleted showProjectsCounters function
    This function broke the application

--- src/UI/Views/showCounters.js
+++ src/UI/Views/showCounters.js
@@ -2,14 +2,12 @@ import inboxCounter from "./inboxCounter";
 import nextCounter from "./nextCounter";
 import todayCounter from "./todayCounter";
 import tomorrowCounter from "./tomorrowCounter";
-import showProjectCounter from "./showProjectsCounters";
 
 const showCounters = () => {
   inboxCounter();
   todayCounter();
   tomorrowCounter();
   nextCounter();
-  showProjectCounter();
 };
 
 export default showCounters;
